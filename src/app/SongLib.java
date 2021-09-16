a89c14e2b0c1eb4b1b4847917868f112413fca5a
Apurva Narde <apurva.narde@rutgers.edu>
2021-09-16 10:46:05 -0400

    Init

---
+++ src/app/SongLib.java
@@ -0,0 +1,17 @@
+package SongLibrary.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class SongLib extends Application{
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/SongLibrary/view/SongLib.fxml"));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/view/SongLibController.java
@@ -0,0 +1,10 @@
+package SongLibrary.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SongLibController {
+
+}
