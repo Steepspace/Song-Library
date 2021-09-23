517c92f570d7281dab5618336958c44bafdadd2a
Max Geiger <max.geiger@rutgers.edu>
2021-09-23 05:20:22 -0400

    BUG FIX REQUIRED

--- src/songlib/app/SongLib.java
+++ src/songlib/app/SongLib.java
@@ -3,12 +3,24 @@ package songlib.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import songlib.view.SongLibController;
 
 public class SongLib extends Application{
 
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/songlib/view/SongLib.fxml"));
+        AnchorPane root = (AnchorPane)loader.load();
+
+		SongLibController songController = loader.getController();
+		songController.start();
+
+		Scene scene = new Scene(root, 200, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show(); 
+
     }
 
     public static void main(String[] args) {
--- src/songlib/view/SongLibController.java
+++ src/songlib/view/SongLibController.java
@@ -1,10 +1,20 @@
 package songlib.view;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
 
 public class SongLibController {
-
+	@FXML
+	ListView<String> listView;
+	
+	private ObservableList<String> obsList;
+	
+	public void start(){
+		obsList = FXCollections.observableArrayList("1","2","3");
+		listView.setItems(obsList);
+	}
 }
