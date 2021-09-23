adbc8912d815f4604b67e8bfe00b751a19d475e2
Max Geiger <max.geiger@rutgers.edu>
2021-09-23 17:37:11 -0400

    Implemented initial read and ListView functionality changes in SongList automatically updated in list

--- src/songlib/view/SongLibController.java
+++ src/songlib/view/SongLibController.java
@@ -1,20 +1,27 @@
 package songlib.view;
 
+import java.util.ArrayList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import songlib.io.*;
 
 
 
 public class SongLibController {
 	@FXML
-	ListView<String> listView;
-	
-	private ObservableList<String> obsList;
+	ListView<Song> listView;
+	SongLibIO IO = new SongLibIO("../data/data.csv");//change to generic path variable
+	//Song test = new Song("huh","mg");
+		
+	private ObservableList<Song> SongList;
 	
 	public void start(){
-		obsList = FXCollections.observableArrayList("1","2","3");
-		listView.setItems(obsList);
+		//initial read from file
+		SongList = FXCollections.observableArrayList(IO.read());
+		//insert items into listview
+		listView.setItems(SongList);
+		SongList.add(new Song("test","an"));
 	}
 }
