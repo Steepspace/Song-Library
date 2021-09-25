91af1416fed659ff4210b53ea579629d0a05b24c
Max Geiger <max.geiger@rutgers.edu>
2021-09-25 02:41:44 -0400

    Basic button layout & removing unneccesary code

--- src/songlib/view/SongLibController.java
+++ src/songlib/view/SongLibController.java
@@ -2,9 +2,11 @@ package songlib.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import songlib.io.*;
 
 
@@ -15,6 +17,9 @@ public class SongLibController {
 	@FXML Text artText;
 	@FXML Text albText;
 	@FXML Text yText;
+	@FXML Button addB;
+	@FXML Button delB;
+	@FXML Button editB;
 	
 	SongLibIO IO = new SongLibIO("../data/data.csv");//change to generic path variable
 	//Song test = new Song("huh","mg");
@@ -32,11 +37,19 @@ public class SongLibController {
 		listView.getSelectionModel().selectedIndexProperty().addListener( (obs, oldVal, newVal) -> songDetail());
 	}
 
-	private void songDetail() {
+	private void songDetail(){
 		Song selected = listView.getSelectionModel().getSelectedItem();
 		songText.setText(selected.getName());
 		artText.setText(selected.getArtist());
 		albText.setText(selected.getAlbum());
 		yText.setText(selected.getYear().toString());
 	}
+	
+	public void addSong(ActionEvent e){
+		System.out.print("hello");
+	}
+	
+	public void deleteSong(ActionEvent e){}
+	
+	public void editSong(ActionEvent e){}
 }
