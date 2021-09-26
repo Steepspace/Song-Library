6389d1822f722d8daf5f64c8f0a71cf3e22130f4
Apurva Narde <apurva.narde@rutgers.edu>
2021-09-26 15:59:42 -0400

    SongLibIO
    - Switching from using ArrayList to ObservableList
    - reading the file in the constructor only
    - changing add and update to boolean methods
    - removing initial checks in add and update as those are taken care of by the controller classes
    - getList() method to return the songList
    - write() changed to only checking if the songList is null, fixed a critical bug not allowing to delete the last element in the list view

--- src/songlib/io/SongLibIO.java
+++ src/songlib/io/SongLibIO.java
@@ -1,5 +1,7 @@
 package songlib.io;
 
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,86 +10,81 @@ import java.io.File;
 
 public class SongLibIO {
 
-    private ArrayList<Song> songList;
+    private ObservableList<Song> songList;
     private String path;
 
-    public SongLibIO(){
-        songList = new ArrayList<>();
-    }
-
     public SongLibIO(final String path){
-        this();
         this.path = path;
+        read();
     }
 
     // 0 if successfully added
     // 1 if either name or artist is null or year is nonpositive
     // 2 if song already exists
-    public int add(final Song song){
-        if (song == null ||
-            song.getName() == null ||
-            song.getArtist() == null ||
-            (song.getYear() != null && song.getYear() < 1)) return 1;
+    public boolean add(final Song song){
+        // if (song == null ||
+        //     song.getName() == null ||
+        //     song.getArtist() == null ||
+        //     (song.getYear() != null && song.getYear() < 1)) return 1;
 
-        songList = read();
+        // // Remove any trailing or leading white spaces
+        // song.setName(song.getName().strip());
+        // song.setArtist(song.getArtist().strip());
 
-        // Remove any trailing or leading white spaces
-        song.setName(song.getName().strip());
-        song.setArtist(song.getArtist().strip());
+        // if (song.getAlbum() != null) song.setAlbum(song.getAlbum().strip());
 
-        if (song.getAlbum() != null) song.setAlbum(song.getAlbum().strip());
-
-        if(songList.contains(song)) return 2;
+        if(songList.contains(song)) return false;
 
         songList.add(song);
 
         write();
-        return 0;
+        return true;
     }
 
     // 0 if successfully updated
     // 1 if either name or artist is null or year is nonpositive
     // 2 if newSong already exists in the data
     // 3 if oldSong does not exist in the data
-    public int update(final Song oldSong, final Song newSong){
-        if (newSong == null ||
-            newSong.getName() == null ||
-            newSong.getArtist() == null ||
-            (newSong.getYear() != null && newSong.getYear() < 1)) return 1;
+    public boolean update(final Song oldSong, final Song newSong){
+        // if (newSong == null ||
+        //     newSong.getName() == null ||
+        //     newSong.getArtist() == null ||
+        //     (newSong.getYear() != null && newSong.getYear() < 1)) return 1;
 
-        songList = read();
+        // // Remove any trailing or leading white spaces
+        // newSong.setName(newSong.getName().strip());
+        // newSong.setArtist(newSong.getArtist().strip());
 
-        // Remove any trailing or leading white spaces
-        newSong.setName(newSong.getName().strip());
-        newSong.setArtist(newSong.getArtist().strip());
+        // if (newSong.getAlbum() != null) newSong.setAlbum(newSong.getAlbum().strip());
 
-        if (newSong.getAlbum() != null) newSong.setAlbum(newSong.getAlbum().strip());
-
-        if (!oldSong.equals(newSong) && songList.contains(newSong)) return 2;
+        if (!oldSong.equals(newSong) && songList.contains(newSong)) return false;
 
         final int index = songList.indexOf(oldSong);
 
-        if (index == -1) return 3;
+        // if (index == -1) return 3;
 
         songList.set(index, newSong);
 
         write();
-        return 0;
+        return true;
     }
 
     // Remove song if exists
     public void delete(final Song song){
-        songList = read();
-
         songList.remove(song);
 
         write();
     }
 
-    public ArrayList<Song> read(){
+    public ObservableList<Song> getList(){
+        return songList;
+    }
+
+    private void read(){
         try {
             final BufferedReader csvReader = new BufferedReader(new FileReader(this.path));
-            songList.clear();
+
+            songList = FXCollections.observableArrayList(new ArrayList<>());
 
             String row;
             while ((row = csvReader.readLine()) != null) {
@@ -104,22 +101,20 @@ public class SongLibIO {
             }
             csvReader.close();
         } catch (final Exception e) {
-            return null;
+            songList = null;
         }
-
-        return songList;
     }
 
     public void clear(){
         try {
-        final File file = new File(path);
-        file.delete();
-        file.createNewFile();
+            final File file = new File(path);
+            file.delete();
+            file.createNewFile();
         } catch (final Exception e) {}
     }
 
     private void write(){
-       if(songList == null || songList.isEmpty()) return;
+       if(songList == null) return;
        songList.sort(null);
 
         try {
